64db7b4670a7a7ca43e767355fad1aee4f0150c7
Nefko <marciusneven@gmail.com>
2020-11-08 02:33:43 +0100

    asd

---
+++ scripts/script.js
@@ -0,0 +1,56 @@
+$(window).scroll(function () {
+
+    $('.animacija-prva').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight*1 ) {
+            $(this).addClass("slideRight");
+            if ($(this).hasClass("slideLeft"))   $(this).removeClass("slideLeft");
+
+        } else {
+
+        if ($(this).hasClass("slideRight"))   $(this).addClass("slideLeft");
+
+            $(this).removeClass("slideRight");
+
+        }
+    });
+
+    $('.animacija-druga').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight*1 ) {
+
+            $(this).addClass("fadeIn");
+              if ($(this).hasClass("fadeOut"))   $(this).removeClass("fadeOut");
+
+
+        } else {
+
+       if ($(this).hasClass("fadeIn"))   $(this).addClass("fadeOut");
+
+            $(this).removeClass("fadeIn");
+
+        }
+    });
+
+    $('.animacija-treca').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight*0.8 ) {
+            $(this).addClass("vrti");
+
+        } else {
+
+            $(this).removeClass("vrti");
+
+        }
+    });
+
+});
